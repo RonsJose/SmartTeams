ea14cc7aab2769ce58b666c6640dfce8d84cdb3e
SuperButler <g00467621@atu.ie>
2025-11-14 03:43:39 +0000

    tetris_blocks
    made a few classes for the blocks and their positions and how they will look on the grid

--- tetris.py
+++ tetris.py
@@ -1,28 +1,28 @@
-import pygame 
-import sys
+import pygame, sys
 from tetris_grid import Grid
+from tetris_blocks import *
 
 pygame.init()
 dark_blue = (44, 44, 127)
 
-#display setup
-screen = pygame.display.set_mode((300, 600))
+screen = pygame.display.set_mode((300, 600)) #display setup
 pygame.display.set_caption("Python Tetris")
 
 clock = pygame.time.Clock()
 
-game_grid = Grid()
-game_grid.print_grid()
+game_grid = Grid() #calls class
 
-#game loop
-while True:
+block = TBlock()
+
+while True: #game loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    #drawing
-    screen.fill(dark_blue)
+    
+    screen.fill(dark_blue) #drawing
     game_grid.draw(screen)
+    block.draw(screen)
 
     pygame.display.update()
     clock.tick(60)
