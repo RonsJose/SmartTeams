9f2047a9f9d470ce4f33f53af986579967eb47e3
c0nor-S <150271462+c0nor-S@users.noreply.github.com>
2025-12-10 14:25:13 +0000

    Fixed Flash And HTML/CSS Not Working Correctly

--- currency.py
+++ currency.py
@@ -1,27 +1,10 @@
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
-<<<<<<< HEAD
-import requests
-=======
->>>>>>> 1c29c59 (Fixed Flask And HTML/CSS Not Working Correctly)
 import os
 import requests
 
 app = Flask(__name__)
 
-<<<<<<< HEAD
-
-load_dotenv()
-API_KEY=os.getenv("API_KEYC")
-
-def currency_get(amount = 0, from_currency = "EUR", to_currency = "EUR"):
-    if amount is None or from_currency is None or to_currency is None:
-        return None
-    BASE_URL = f"https://v6.exchangerate-api.com/v6/{API_KEY}/latest/{from_currency}"
-    amount=float(amount)
-    result = None
-    converted = None
-=======
 CURRENCIES = ["USD", "EUR", "GBP", "JPY", "AUD", "CAD", "CHF", "CNY", "NZD"]
 
 load_dotenv()
@@ -29,20 +12,10 @@ API_KEY = os.getenv("API_KEY")
 
 def currency_get(amount=0, from_currency="EUR", to_currency="EUR"):
     BASE_URL = f"https://v6.exchangerate-api.com/v6/{API_KEY}/latest/{from_currency}/"
->>>>>>> 1c29c59 (Fixed Flask And HTML/CSS Not Working Correctly)
     response = requests.get(BASE_URL)
     data = response.json()
 
     if data["result"] != "success":
-<<<<<<< HEAD
-        result = "Error fetching rates"
-        converted = None
-    else:
-        rates = data["conversion_rates"]
-        converted = round(amount * rates[to_currency], 2)
-
-    return converted
-=======
         return "Error fetching rates"
     rates = data["conversion_rates"]
     return round(amount * rates[to_currency], 2)
@@ -59,4 +32,3 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
->>>>>>> 1c29c59 (Fixed Flask And HTML/CSS Not Working Correctly)
